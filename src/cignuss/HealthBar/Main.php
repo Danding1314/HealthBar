8a546e162b04f30484e2527aed0dc642db4e281b
Eugene Lind <47429448+Liiindth@users.noreply.github.com>
2020-01-04 20:09:49 +0300

    minor update

--- src/cignuss/HealthBar/Main.php
+++ src/cignuss/HealthBar/Main.php
@@ -2,19 +2,18 @@
 
 declare(strict_types=1);
 
-namespace cignuss;
+namespace cignuss\HealthBar;
 
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat;
 
-class HealthBar extends PluginBase{
+class Main extends PluginBase{
 
 	public function onEnable() : void{
 		$this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(function(int $currentTick) : void{
 			foreach($this->getServer()->getOnlinePlayers() as $player){
-				if($player->getGamemode() !== Player::SURVIVAL){
+				if(!$player->isSurvival() || !$player->isAdventure()){
 					continue;
 				}
 				$player->setScoreTag(TextFormat::RESET . TextFormat::WHITE . round($player->getHealth() / 2, 2) . TextFormat::RED . " ❤");
